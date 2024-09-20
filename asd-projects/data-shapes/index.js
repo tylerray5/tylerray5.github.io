6504e2cc1dc2bd6bb145ee719ed9cbe5eb3fd013
tylerray5 <tyler.simien@ekhs.org>
2024-09-20 01:00:31 +0000

    worked

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -36,12 +36,18 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
-
+// TODO 0 complete//
   // TODO 1: create a new shape object and add it to the array
-  
-
+  var shape ={ 
+    circle: "black",
+  shape: " circle",
+  repeat: 3,
+  };
+  dataShapes.push(shape);
   // TODO 2: add a new property to all data shapes
-  
+ for(var i=0; i <  dataShapes.length; i++){
+var currentShape = dataShapes;
+ }
 
   // TODO 3-a: add a function that handles the static display type
   
