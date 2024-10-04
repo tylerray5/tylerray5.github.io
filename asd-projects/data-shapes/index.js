e34eac49c3c76a0ff31e59ea7a6f95ac1dd0aa81
tylerray5 <tyler.simien@ekhs.org>
2024-10-04 00:53:34 +0000

    worked on data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -56,20 +56,23 @@ $(document).ready(function () {
       currentShape.goodBehavior = "spin";
     }
   }
-console.log(dataShapes);
+  console.log(dataShapes);
   // TODO 3-a: add a function that handles the static display type
-function handleStatic(data){
-  setBackgroundWithObject(data);
-  animationDetails.displayType =1;
-}
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1;
+  }
   // TODO 4-a: add a function that handles the good display type
-function handleGood(color ){
-
-}
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+  }
   // TODO 5-a: add a function that handles the bad display type
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
+  var currentShape =handleStatic(dataShapes[currentIndex])
+
   /////////////////////////////////////////////////
 
   function staticDisplay() {
@@ -80,6 +83,8 @@ function handleGood(color ){
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
     //var currentShape = dataShapes[currentIndex];
+    //handleGood.currentShape(color ,  shape, repeat)
+    
   }
 
   function badDisplay() {
