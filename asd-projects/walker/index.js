d84a34046bedfbbcadb30f6ff49182f22aeb055a
tylerray5 <tyler.simien@ekhs.org>
2024-12-06 02:02:18 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -23,6 +23,7 @@ function handleKeyUp (){}
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL); // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on("keydown", handleKeyDown); // change 'eventType' to the type of event you want to handle
   $(document).on("keyup", handleKeyUp);
+
   var walker = {
     positionX: 5,
     positionY: 5,
@@ -41,6 +42,7 @@ function handleKeyUp (){}
   */
   function newFrame() {
     repositionGameItem ()
+    wallCollision ()
     redrawGameItem()
     
   }
@@ -62,14 +64,32 @@ function handleKeyUp (){}
       walker.speedY = 5;
     }
   }
+
+  function handleKeyUp(event){
+    if (event.which === KEY.LEFT){
+      walker.speedX = 0;
+    }
+    if (event.which === KEY.RIGHT) {
+      walker.speedX = 0;
+    }
+    if (event.which === KEY.UP) {
+      walker.speedY = 0;
+    }
+    if (event.which === KEY.DOWN) {
+      walker.speedY = 0;
+    }
+  }
+
   function repositionGameItem() {
     walker.positionX = walker.positionX + walker.speedX;
     walker.positionY = walker.positionY + walker.speedY;
   }
+
   function redrawGameItem(newFrame) {
     $("#walker").css("left" , walker.positionX);
-    $("#walker").css("top", walker.positionY)
+    $("#walker").css("top", walker.positionY);
   }
+
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -81,4 +101,20 @@ function handleKeyUp (){}
     // turn off event handlers
     $(document).off();
   }
+  function wallCollision(){
+    const bottomSide = $("#board").height() - $("#walker").height()
+    const rightSide = $("#board").width() - $("#walker").width()
+   
+    if (walker.coordinateX < $("#board").width()){
+      walker.coordinateX = $("#board").widht();
+    }
+    else if (walker.positionX < 0 ) {
+      walker.positionX = 0;
+    }
+    if (walker.coordinateY < $("#board").height()){
+     // walker.coordinateY = 
+    }
+   //else if ("board"){}
+  }
+
 }
